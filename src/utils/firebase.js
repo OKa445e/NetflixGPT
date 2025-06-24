a15692e85600fedf3eb3ba14cb9a93b9094ae5c6
Atharv sinha <atharvdav@gmail.com>
2025-06-24 12:05:40 +0530

    private keys

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; //
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMSp1Tunq6LP6PjyUBnUUmUyYtoYFiVAM",
-  authDomain: "netflixgpt-37da5.firebaseapp.com",
-  projectId: "netflixgpt-37da5",
-  storageBucket: "netflixgpt-37da5.firebasestorage.app",
-  messagingSenderId: "893947786851",
-  appId: "1:893947786851:web:4a7cefbc7529b979152a56",
-  measurementId: "G-Z5W9XKL77Y"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const auth = getAuth(app); // Pass 'app' here
+export const auth = getAuth(app);
